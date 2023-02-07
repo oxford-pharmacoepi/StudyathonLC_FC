b739e40231453d517f990d7f9c3cb7af2d9e3979
KimLopezGuell <kimlopezguell@gmail.com>
2023-02-07 15:14:50 +0000

    Updated tempDir

--- RunStudy.R
+++ RunStudy.R
@@ -2,10 +2,19 @@
 if (!file.exists(output.folder)){
   dir.create(output.folder, recursive = TRUE)}
 
+# Create zip file
+zipName <- paste0(db.name,"_FCresults")
+tempDir <- zipName
+tempDirCreated <- FALSE
+if (!dir.exists(tempDir)) {
+  dir.create(tempDir)
+  tempDirCreated <- TRUE
+}
+
 start <- Sys.time()
 
 # Start log
-log_file <- paste0(output.folder, "/log.txt")
+log_file <- paste0(tempDir, "/log.txt")
 logger <- create.logger()
 logfile(logger) <- log_file
 level(logger) <- "INFO"
@@ -23,15 +32,7 @@ longcovidCounts <- cdm[[cohort_table_name]] %>%
   right_join(FC_cohorts, by = c("cohort_definition_id"="cohortId")) %>% mutate(n = as.numeric(n)) %>% mutate(n = if_else(is.na(n), 0, n)) %>% mutate(n = ifelse(n <= 5, NA, n)) %>% select(cohortName,n)
 info(logger, 'GOT COUNTS')
 
-# Create zip file
-zipName <- paste0(db.name,"_FCresults")
-tempDir <- zipName
-tempDirCreated <- FALSE
-if (!dir.exists(tempDir)) {
-  dir.create(tempDir)
-  tempDirCreated <- TRUE
-}
-
+# Export csv
 write.csv(longcovidCounts,
           file = file.path(
             tempDir,
@@ -40,15 +41,16 @@ write.csv(longcovidCounts,
           row.names = FALSE
 )
 
+info(logger, 'SAVED RESULTS IN THE OUTPUT FOLDER')
+Sys.time() - start
+readLines(log_file)
+
 zip::zip(zipfile = file.path(output.folder, paste0(zipName, ".zip")),
          files = list.files(tempDir, full.names = TRUE))
 if (tempDirCreated) {
   unlink(tempDir, recursive = TRUE)
 }
-info(logger, 'SAVED RESULTS IN THE OUTPUT FOLDER')
 
 print("Done!")
 print("If all has worked, there should now be a zip file with your results in the output folder to share")
 print("Thank you for running the study!")
-Sys.time() - start
-readLines(log_file)
